08eddcb93e8f33797a7ac66db7ee9123685ebef3
bodhikara <bodhikara@hotmail.com>
2024-10-18 13:23:00 -0400

    trying css

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const mainDiv = document.querySelector("main-div");
-const createdDiv = document.createElement("div");
-createdDiv.textContent = "hello"
+
 for (i = 0; i < 16; i++){
+    const createdDiv = document.createElement("div");
+    createdDiv.classList.add("div-squares");
     mainDiv.appendChild(createdDiv);
 }
